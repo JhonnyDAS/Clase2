0cbc259b56cb3814c9ee9b1477e3cdda46017c7f
Jhonny <jhonnla27@gmail.com>
2018-03-05 22:46:05 -0400

    Primeros Pasos

---
+++ Clase2/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Clase2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rbMujer = new System.Windows.Forms.RadioButton();
+            this.rbVaron = new System.Windows.Forms.RadioButton();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbExp = new System.Windows.Forms.ComboBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.cNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cApellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cCi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cGenero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cDireccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(73, 16);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(160, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(73, 42);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(160, 20);
+            this.txtApellido.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Apellidos";
+            // 
+            // txtCi
+            // 
+            this.txtCi.Location = new System.Drawing.Point(73, 68);
+            this.txtCi.Name = "txtCi";
+            this.txtCi.Size = new System.Drawing.Size(95, 20);
+            this.txtCi.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CI";
+            // 
+            // rbMujer
+            // 
+            this.rbMujer.AutoSize = true;
+            this.rbMujer.Location = new System.Drawing.Point(73, 94);
+            this.rbMujer.Name = "rbMujer";
+            this.rbMujer.Size = new System.Drawing.Size(51, 17);
+            this.rbMujer.TabIndex = 6;
+            this.rbMujer.TabStop = true;
+            this.rbMujer.Text = "Mujer";
+            this.rbMujer.UseVisualStyleBackColor = true;
+            // 
+            // rbVaron
+            // 
+            this.rbVaron.AutoSize = true;
+            this.rbVaron.Location = new System.Drawing.Point(130, 94);
+            this.rbVaron.Name = "rbVaron";
+            this.rbVaron.Size = new System.Drawing.Size(53, 17);
+            this.rbVaron.TabIndex = 7;
+            this.rbVaron.TabStop = true;
+            this.rbVaron.Text = "Varon";
+            this.rbVaron.UseVisualStyleBackColor = true;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(73, 117);
+            this.txtDireccion.Multiline = true;
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(160, 75);
+            this.txtDireccion.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Direccion";
+            // 
+            // cmbExp
+            // 
+            this.cmbExp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbExp.FormattingEnabled = true;
+            this.cmbExp.Items.AddRange(new object[] {
+            "Or",
+            "Lp",
+            "Cbb",
+            "Bn",
+            "Pd",
+            "Sc",
+            "Pt",
+            "Tj",
+            "Ch"});
+            this.cmbExp.Location = new System.Drawing.Point(174, 68);
+            this.cmbExp.Name = "cmbExp";
+            this.cmbExp.Size = new System.Drawing.Size(59, 21);
+            this.cmbExp.TabIndex = 10;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(735, 209);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(654, 209);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 12;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cNombre,
+            this.cApellido,
+            this.cCi,
+            this.cGenero,
+            this.cDireccion});
+            this.dgvLista.Location = new System.Drawing.Point(240, 16);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.Size = new System.Drawing.Size(565, 176);
+            this.dgvLista.TabIndex = 13;
+            // 
+            // cNombre
+            // 
+            this.cNombre.HeaderText = "Nombre";
+            this.cNombre.Name = "cNombre";
+            // 
+            // cApellido
+            // 
+            this.cApellido.HeaderText = "Apellido";
+            this.cApellido.Name = "cApellido";
+            // 
+            // cCi
+            // 
+            this.cCi.HeaderText = "CI";
+            this.cCi.Name = "cCi";
+            // 
+            // cGenero
+            // 
+            this.cGenero.HeaderText = "Genero";
+            this.cGenero.Name = "cGenero";
+            // 
+            // cDireccion
+            // 
+            this.cDireccion.HeaderText = "Direccion";
+            this.cDireccion.Name = "cDireccion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 244);
+            this.Controls.Add(this.dgvLista);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.cmbExp);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rbVaron);
+            this.Controls.Add(this.rbMujer);
+            this.Controls.Add(this.txtCi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rbMujer;
+        private System.Windows.Forms.RadioButton rbVaron;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbExp;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cApellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cCi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cGenero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cDireccion;
+
+    }
+}
+
---
+++ Clase2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clase2
+{
+    public partial class Form1 : Form
+    {
+        string _nombre, _apellido, _genero, _direccion, _ci;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            _nombre = txtNombre.Text;
+            _apellido = txtApellido.Text;
+            _ci = txtCi.Text + "-" + cmbExp.Text;
+            _genero = (rbMujer.Checked == true) ? rbMujer.Text : rbVaron.Text;
+            _direccion = txtDireccion.Text;
+            MessageBox.Show("Nombre: "+_nombre
+                            +"\nApellido: "+_apellido
+                            +"\nCi: "+_ci
+                            +"\nGenero: "+_genero
+                            +"\nDirección: "+_direccion
+                );
+        }
+    }
+}
